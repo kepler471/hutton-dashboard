0d4fe7d95debddddb1111674024d6b8aa2a5d609
Stelios Georgiou <steliosdg@gmail.com>
2023-01-03 22:34:57 +0000

    hutton criteria calculation complete

--- app.R
+++ app.R
@@ -22,11 +22,20 @@ site_data <- map_dfr(
 
 site_data_fmt <-
   site_data %>%
-  left_join(site_metadata, by = c("Site" = "Site_ID")) %>%
-  mutate(ob_time = dmy_hm(ob_time))
+  ## left_join(site_metadata, by = c("Site" = "Site_ID")) %>%
+  mutate(ob_time = date(dmy_hm(ob_time))) %>%
+  select(-c(day, month))
 
 hutton <- function(.data) {
-  .data %>%
-    group_by(hour, month, day) %>%
-
+  site_data_fmt %>%
+    group_by(Site, ob_time) %>%
+    summarise(
+      min_temp = min(air_temperature),
+      humid_hours = sum(rltv_hum >= 90.0)
+    ) %>%
+    mutate(
+        warm = pmin(lag(min_temp, n = 1), lag(min_temp, n = 2)) >= 10,
+        humid = pmin(lag(humid_hours, n = 1), lag(humid_hours, n = 2)) >= 6
+    ) %>%
+    ungroup()
 }
